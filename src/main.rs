56ed96d1e0cde1bac46bf59f5bfae0a9e76c913c
0xSchnappi <952768182@qq.com>
2024-08-24 15:29:30 +0800

    案例二：异步任务超时处理

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use std::time::Duration;
 use reqwest::{self, StatusCode};
+use tokio::time::{sleep, timeout};
 
 #[tokio::main]
 async fn main() {
+    // 案例1：异步HTTP请求
     let mut urls = vec![];
-
     urls.push("https://www.baidu.com"); // 百度
     urls.push("https://www.qq.com"); // 腾讯
     urls.push("https://www.alibaba.com"); // 阿里巴巴
@@ -19,6 +21,17 @@ async fn main() {
         let status_code = get_http_status(url).await;
         println!("url:{}\tstatus code:{}", url, status_code);
     }
+
+    // 案例2：异步任务超时
+    let timeout_duration = Duration::from_secs(2);
+
+    // 启动超时任务
+    for i in 0..5 {
+        match timeout(timeout_duration, timeout_async(Duration::from_secs(i))).await {
+            Ok(_) => println!("Task [{}] completed successfully", i),
+            Err(_) => println!("Task [{}] timed out", i),
+        }
+    }
 }
 
 async fn get_http_status(url: &str) -> StatusCode {
@@ -30,4 +43,8 @@ async fn get_http_status(url: &str) -> StatusCode {
         }
     };
     return status_code;
+}
+
+async fn timeout_async(time_date: Duration) {
+    sleep(time_date).await;
 }
