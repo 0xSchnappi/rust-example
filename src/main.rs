7515ccb98f39766e30bb1fa50a8c5e8f659d1e1b
0xSchnappi <952768182@qq.com>
2024-08-24 11:49:48 +0800

    案例1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::{self, StatusCode};
+
+#[tokio::main]
+async fn main() {
+    let mut urls = vec![];
+
+    urls.push("https://www.baidu.com"); // 百度
+    urls.push("https://www.qq.com"); // 腾讯
+    urls.push("https://www.alibaba.com"); // 阿里巴巴
+    urls.push("https://www.taobao.com"); // 淘宝
+    urls.push("https://www.jd.com"); // 京东
+    urls.push("https://www.sina.com.cn"); // 新浪
+    urls.push("https://www.163.com"); // 网易
+    urls.push("https://www.sohu.com"); // 搜狐
+    urls.push("https://baike.baidu.com"); // 百度百科
+    urls.push("https://www.toutiao.com"); // 今日头条
+
+    for url in urls.iter() {
+        let status_code = get_http_status(url).await;
+        println!("url:{}\tstatus code:{}", url, status_code);
+    }
+}
+
+async fn get_http_status(url: &str) -> StatusCode {
+    let status_code = match reqwest::get(url).await {
+        Ok(resp) => resp.status(),
+        Err(e) => {
+            println!("error: {}", e);
+            e.status().unwrap()
+        }
+    };
+    return status_code;
 }
